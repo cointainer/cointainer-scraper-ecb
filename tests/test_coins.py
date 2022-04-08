059a259313c061cd27c2c8770e2210a94abbe745
B4rtware <34386047+B4rtware@users.noreply.github.com>
2022-04-08 18:03:07 +0200

    test: add additional tests

--- tests/test_coins.py
+++ tests/test_coins.py
@@ -11,7 +11,19 @@ def test_coins_2004():
 
 def test_coins_2007():
     with open("tests/data/comm_2007.en.html", "rb") as f:
-        coins = _get_commemorative_coins(f.read(), "en", 2004)
+        coins = _get_commemorative_coins(f.read(), "en", 2007)
 
         assert len(coins) == 8
         assert len(coins[5].coinages) == 13
+
+def test_coins_2015():
+    with open("tests/data/comm_2015.en.html", "rb") as f:
+        coins = _get_commemorative_coins(f.read(), "en", 2015)
+
+        assert len(coins) == 29
+
+def test_coins_2021():
+    with open("tests/data/comm_2021.en.html", "rb") as f:
+        coins = _get_commemorative_coins(f.read(), "en", 2021)
+
+        assert len(coins) == 30
