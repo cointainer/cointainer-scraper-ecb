9261172912dada8e7eed72cde6bd60dc0e52875d
B4rtware <34386047+B4rtware@users.noreply.github.com>
2022-04-08 02:37:51 +0200

    refactor: major refactor the overall parsing structure

--- coin_collector_parser_ecb/coins.py
+++ coin_collector_parser_ecb/coins.py
@@ -2,9 +2,9 @@ import datetime
 import json
 import logging
 from dataclasses import dataclass, field
-from enum import IntEnum
 from pathlib import Path
-from typing import Dict, List, cast
+import sys
+from typing import Any, Dict, List, Optional, Tuple, cast, Union
 from bs4.element import NavigableString, Tag
 
 import pycountry
@@ -17,7 +17,20 @@ from bs4 import BeautifulSoup
 
 COIN_IMAGE_URL_REGEX = ""
 
-LOG = logging.getLogger(__name__)
+LOG = logging.getLogger("coin_collector")
+LOG.setLevel(logging.INFO)
+
+formatter = logging.Formatter(
+    "[{asctime}] [{levelname:<7}] [{name}] {message}",
+    datefmt="%d.%m.%Y %H:%M:%S",
+    style="{",
+)
+
+import pprint
+
+stream = logging.StreamHandler(sys.stdout)
+stream.setFormatter(formatter)
+LOG.addHandler(stream)
 
 ECB_BASE_URL = "https://www.ecb.europa.eu"
 ECB_TWO_EURO_URL = (
@@ -39,26 +52,68 @@ info_description_mapping = {
     3: "issuing date",
 }
 
+million_mapping = {
+    "милиона монети",
+    "milionů mincí",
+    "mio. mønter",
+    "Millionen Münzen",
+    "εκατομμύρια κέρματα",
+    "million coins",
+    "millones de monedas",
+    "miljonit münti",
+    "miljoonaa kolikkoa",
+    "millions de pièces",
+    "000 000 kovanica",
+    "millió érme",
+    "milioni di pezzi",
+    "mln. monetų",
+    "milj. monētu",
+    "miljun munita",
+    "miljoen munten",
+    "milionów monet",
+    "milhões de moedas",
+    "milioane de monede",
+    "miliónov mincí",
+    "milijonov kovancev",
+    "miljoner mynt",
+}
+
+
+@dataclass
+class Coinage:
+    country: Optional[str]
+    image_default_url: Optional[str]
+    volume: Optional[int]
+    circulation_date: Optional[datetime.date] = None
+    image_attribution: Optional[str] = None
+    circulation_date_info: Optional[str] = None
+    volume_info: Optional[str] = None
+
 
 @dataclass
 class TwoEuro:
-    countries: List[str] = field(default_factory=list)
     feature: str = ""
-    volume: int = 0
+    # volume: int = 0
+    # volume_info: str = ""
     description: str = ""
-    image_urls: List[str] = field(default_factory=list)
-    image_attributions: List[str] = field(default_factory=list)
-    circulation_dates: List[datetime.date] = field(default_factory=list)
+    # countries: List[str] = field(default_factory=list)
+    # image_urls: List[str] = field(default_factory=list)
+    # image_attributions: List[str] = field(default_factory=list)
+    # circulation_dates: List[datetime.date] = field(default_factory=list)
+    # circulation_dates_info: str = ""
+    coinages: List[Coinage] = field(default_factory=list)
 
     def __str__(self) -> str:
         return (
-            f"Coin(countries='{self.countries}', "
+            f"Coin(countries='{[coinage for coinage in self.coinages]}', "
             f"feature='{self.feature[:25]}...', "
-            f"volume='{self.volume}', "
             f"description='{self.description[:25]}...')"
         )
 
-def _get_alpha2_country_from_string(text: str, translate: bool = True) -> str:
+
+def _get_alpha2_country_from_string(
+    text: str, year: int, paragraph_index: int, translate: bool = True
+) -> Union[str, None]:
     country_name = text.strip().capitalize()
     if translate:
         country_name = country_translations[country_name.lower()]
@@ -70,102 +125,285 @@ def _get_alpha2_country_from_string(text: str, translate: bool = True) -> str:
         try:
             found_country = fuzzy_search_cache[capitalized_translated_country]
         except KeyError:
-            found_countries = pycountry.countries.search_fuzzy(
-                capitalized_translated_country
-            )
-            if not found_countries:
-                LOG.warning(
-                    f"no country object matched country named: '{country_name}'"
+            try:
+                found_countries = pycountry.countries.search_fuzzy(
+                    capitalized_translated_country
                 )
+            except LookupError:
+                LOG.warning(
+                    f"({year}, {paragraph_index}) no country object matched country "
+                    f"named: '{country_name}'"
+                )
+                return None
             found_country = found_countries[0]
             LOG.info(
-                "found country '{found_country.name}' with fuzzy search from '{translated_country}'"
+                f"({year}, {paragraph_index}) found country '{found_country.name}' "
+                f"with fuzzy search from '{capitalized_translated_country}'"
             )
             fuzzy_search_cache[capitalized_translated_country] = found_country
 
     return found_country.alpha_2
 
 
-def get_commemorative_coins(lang: str = "", year: int = START_YEAR) -> List[TwoEuro]:
+def get_commemorative_coins(lang: str = "en", year: int = START_YEAR) -> List[TwoEuro]:
     response = requests.get(ECB_TWO_EURO_URL.format(year=year, lang=lang))
-    soup = BeautifulSoup(response.content, "html.parser")
+    return _get_commemorative_coins(response.content, lang, year)
+
+
+def _create_unknown_fall_back_country(
+    year: int,
+    paragraph_index: int,
+    title_header: str,
+    image_url: str,
+    circulation_date: Optional[datetime.date],
+    circulation_date_info: Optional[str],
+    volume: Optional[int],
+    volume_info: Optional[str],
+) -> Coinage:
+    LOG.warning(
+        f"({year}, {paragraph_index}) failed to parse "
+        f"fall back country name from h3 tag "
+        f"with content: '{title_header}'. Country "
+        "will be 'UNKNOWN_COUNTRY'"
+    )
+    # add coinage with unknown country
+    return Coinage(
+        country="COUNTRY_UNKNOWN",
+        image_default_url=image_url,
+        image_attribution="",
+        circulation_date_info=circulation_date_info,
+        circulation_date=circulation_date,
+        volume=volume,
+        volume_info=volume_info,
+    )
+
+
+@dataclass
+class Content:
+    feature: str = ""
+    description: str = ""
+    raw_issuing_volume: str = ""
+    raw_issuing_date: str = ""
+
+
+def _parse_content_fields(coin_box: Any, year: int, paragraph_index: int):
+    # get all descriptions fields assumed to be in order:
+    #     'Feature:', 'Description:', 'Issuing volume:', 'Issuing date:'
+    infos: List[str] = []
+    paragraphs = coin_box.find_all("p")
+
+    # description (can be composed of multiple paragraphs)
+    current_paragraph_content = ""
+    while len(paragraphs) > 0:
+        paragraph = paragraphs.pop(0)
+        paragraph_type = paragraph.find("strong")
+
+        if paragraph_type is None:
+            current_paragraph_content += f" {paragraph.text}"
+            continue
+
+        if not paragraph_type.text.strip().endswith(":"):
+            LOG.warning(
+                f"({year}, {paragraph_index}) could not found ':' on strong tag. Possible missing info "
+                f"description: '{info_description_mapping[len(infos)]}' with "
+                f"paragraph tag: {paragraph.text.encode('string_escape')}"
+            )
+            infos.append("")
+            continue
+
+        current_paragraph_content = paragraph.text.lstrip(
+            paragraph_type.text.strip()
+        ).strip()
+
+        infos.append(current_paragraph_content)
+
+    if len(infos) != 4:
+        infos += [""] * (4 - len(infos))
+
+    return Content(
+        feature=infos[0],
+        description=infos[1],
+        raw_issuing_volume=infos[2].strip(),
+        raw_issuing_date=infos[3].strip(),
+    )
+
+
+def _parse_volume(box_content: Content) -> Tuple[Optional[int], Optional[str]]:
+    volume_str = "".join([c for c in box_content.raw_issuing_volume if c.isdigit()])
+    if len(volume_str) != 0:
+        volume = int(volume_str)
+        volume_word_suffix: List[str] = []
+        for word in reversed(
+            box_content.raw_issuing_volume.replace("\xa0", " ").split(" ")
+        ):
+            if any(c.isdigit() for c in word):
+                break
+            volume_word_suffix.append(word)
+
+        if " ".join(reversed(volume_word_suffix)) in million_mapping:
+            volume *= 1_000_000
+        return (volume, None)
+    else:
+        return (None, box_content.raw_issuing_volume)
+
+
+def _parse_circulation_date(
+    box_content: Content, language: str, year: int, paragraph_index: int
+) -> Tuple[Optional[datetime.date], Optional[str]]:
+    parsed_datetime = dateparser.parse(
+        box_content.raw_issuing_date, languages=[language]
+    )
+    if parsed_datetime is not None:
+        return (parsed_datetime.date(), None)
+    else:
+        LOG.warning(
+            f"({year}, {paragraph_index}) could not extract datetime from"
+            f" given circulation date: '{box_content.raw_issuing_date}'. The string will be "
+            f"stored inside the 'circulation_dates_info' attribute."
+        )
+        return (None, box_content.raw_issuing_date)
+
+
+def _get_commemorative_coins(
+    content: bytes, lang: str = "", year: int = START_YEAR
+) -> List[TwoEuro]:
+    soup = BeautifulSoup(content, "html.parser")
     coin_boxes_div = soup.find("div", {"class": "boxes"})
 
     if coin_boxes_div is None:
-        LOG.error("could not find 'div' with class 'boxes'. No coins can be crawled!")
+        LOG.error(
+            f"({year}) could not find 'div' with class 'boxes'. No coins can be crawled!"
+        )
         return []
 
     if coin_boxes_div is NavigableString:
-        LOG.error("soup result is a NavigableString. No coins can be crawled!")
+        LOG.error(
+            f"({year}) soup result is a NavigableString. No coins can be crawled!"
+        )
         return []
 
     coin_boxes = cast(Tag, coin_boxes_div).find_all("div", {"class": "box"})
 
     coins: List[TwoEuro] = []
-    for coin_box in coin_boxes:
-        # get all descriptions fields assumed to be in order:
-        #     'Feature:', 'Description:', 'Issuing volume:', 'Issuing date:'
-        infos: List[str] = []
-        for index, paragraph in enumerate(coin_box.find_all("p")):
-            paragraph_type: str = paragraph.find("strong").text
-            if not paragraph_type.strip().endswith(":"):
-                LOG.warning(
-                    "could not found ':' on strong tag. Possible missing info "
-                    f"description: '{info_description_mapping[index]}' with "
-                    f"paragraph tag: {paragraph.text.encode('string_escape')}"
-                )
-                infos.append("")
-                continue
+    for paragraph_index, coin_box in enumerate(coin_boxes):
+        box_content = _parse_content_fields(coin_box, year, paragraph_index)
 
-            infos.append(paragraph.text.lstrip(paragraph_type).strip())
+        volume, volume_info = _parse_volume(box_content)
 
-        feature = infos[0]
-        description = infos[1]
-        volume = infos[2]
-        raw_datetime = infos[3].strip()
-        parsed_datetime = dateparser.parse(raw_datetime, languages=[lang])
-        circulation_dates = []
-        if parsed_datetime is not None:
-            circulation_dates = [parsed_datetime.date()]
-        else:
-            LOG.warning(
-                "could not extract datetime from"
-                f" given circulation date: '{raw_datetime}'"
-            )
+        circulation_date, circulation_date_info = _parse_circulation_date(
+            box_content, lang, year, paragraph_index
+        )
 
         # coin image(s)
         image_container = coin_box.find("div", {"class": "coins"})
         images = image_container.find_all("img")
 
-        countries: List[str] = []
-        image_urls = [ECB_BASE_URL + image["src"] for image in images]
-        if len(image_urls) > 1:
-            for image_url in image_urls:
-                reminder, country = image_url.rsplit(".", maxsplit=1)[0].rsplit(
-                    "_", maxsplit=1
-                )
-                country_alpha2 = _get_alpha2_country_from_string(country, lang != "en")
-                coin_year = int(reminder.rsplit("_", maxsplit=1)[1])
-                assert coin_year == year
-                countries.append(country_alpha2)
-        else:
-            # coin country
-            title_header = coin_box.find("h3")
-            countries = [
-                _get_alpha2_country_from_string(title_header.text, lang != "en")
-            ]
-
-        coin = TwoEuro(
-            countries=countries,
-            feature=feature,
-            volume=volume,
-            description=description,
-            image_urls=image_urls,
-            circulation_dates=circulation_dates,
+        # coin country
+        title_header = coin_box.find("h3")
+        fall_back_country = _get_alpha2_country_from_string(
+            title_header.text, year, paragraph_index, lang != "en"
         )
-        coins.append(coin)
+
+        image_urls = [
+            ECB_BASE_URL + image["src"].removeprefix(".")
+            for image in images
+            if not image["src"].endswith(".html")
+        ]
+
+        coinages: List[Coinage] = []
+        for image_url in image_urls:
+            country_alpha2 = None
+            # /euro/coins/comm/shared/img/joint_comm_2009_Luxembourg_Face.jpg
+            for word in reversed(
+                image_url.rsplit(".", maxsplit=1)[0]
+                .rsplit("/", maxsplit=1)[1]
+                .split("_")
+            ):
+                country_alpha2 = _get_alpha2_country_from_string(
+                    word, year, paragraph_index, lang != "en"
+                )
+                if country_alpha2 is not None:
+                    break
+
+            if country_alpha2 is None:
+                if not fall_back_country:
+                    coinages.append(
+                        _create_unknown_fall_back_country(
+                            year,
+                            paragraph_index,
+                            title_header,
+                            image_url,
+                            circulation_date,
+                            circulation_date_info,
+                            volume,
+                            volume_info,
+                        )
+                    )
+                else:
+                    # add coinage with fall back country
+                    coinages.append(
+                        Coinage(
+                            country=fall_back_country,
+                            image_default_url=image_url,
+                            image_attribution="",
+                            circulation_date=circulation_date,
+                            circulation_date_info=circulation_date_info,
+                            volume=volume,
+                            volume_info=volume_info,
+                        )
+                    )
+            else:
+                coinages.append(
+                    Coinage(
+                        country=country_alpha2,
+                        image_default_url=image_url,
+                        image_attribution="",
+                        circulation_date=circulation_date,
+                        circulation_date_info=circulation_dates_info,
+                        volume=volume,
+                        volume_info=volume_info,
+                    )
+                )
+
+        if len(image_urls) == 0:
+            if fall_back_country is None:
+                coinages = [
+                    _create_unknown_fall_back_country(
+                        year,
+                        paragraph_index,
+                        title_header,
+                        "",
+                        None,
+                        circulation_dates_info,
+                        volume,
+                        volume_info,
+                    )
+                ]
+            else:
+                LOG.warning(
+                    f"({year}, {paragraph_index}) Could not find any images for "
+                    f"the coin feature: '{box_content.feature}'. Using fall back "
+                    f"country name from h3 tag with text: '{fall_back_country}'"
+                )
+                coinages = [
+                    Coinage(
+                        country=fall_back_country,
+                        image_default_url="",
+                        volume=volume,
+                        volume_info=volume_info,
+                        circulation_date=None,
+                    )
+                ]
+
+        coins.append(
+            TwoEuro(
+                feature=box_content.feature,
+                description=box_content.description,
+                coinages=coinages,
+            )
+        )
     return coins
 
 
 if __name__ == "__main__":
-    get_commemorative_coins("de", year=2006)
+    pprint.pprint(get_commemorative_coins("de", year=2006))
