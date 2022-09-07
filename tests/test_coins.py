d090e61974e0986214e9f690e013e68bebd4a2dc
B4rtware <34386047+B4rtware@users.noreply.github.com>
2022-09-07 11:49:50 +0200

    tests: update to new function name

--- tests/test_coins.py
+++ tests/test_coins.py
@@ -1,16 +1,16 @@
-from coin_collector_scraper_ecb.coins import _get_commemorative_coins
+from coin_collector_scraper_ecb.coins import _get_two_euro_commemorative_coins
 
 
 def test_coins_2004():
     with open("tests/data/comm_2004.en.html", "rb") as f:
-        coins = _get_commemorative_coins(f.read(), "en", 2004)
+        coins = _get_two_euro_commemorative_coins(f.read(), "en", 2004)
 
         assert len(coins) == 6
 
 
 def test_coins_2007():
     with open("tests/data/comm_2007.en.html", "rb") as f:
-        coins = _get_commemorative_coins(f.read(), "en", 2007)
+        coins = _get_two_euro_commemorative_coins(f.read(), "en", 2007)
 
         assert len(coins) == 8
         assert len(coins[5].coinages) == 13
@@ -18,13 +18,13 @@ def test_coins_2007():
 
 def test_coins_2015():
     with open("tests/data/comm_2015.en.html", "rb") as f:
-        coins = _get_commemorative_coins(f.read(), "en", 2015)
+        coins = _get_two_euro_commemorative_coins(f.read(), "en", 2015)
 
         assert len(coins) == 29
 
 
 def test_coins_2021():
     with open("tests/data/comm_2021.en.html", "rb") as f:
-        coins = _get_commemorative_coins(f.read(), "en", 2021)
+        coins = _get_two_euro_commemorative_coins(f.read(), "en", 2021)
 
         assert len(coins) == 30
